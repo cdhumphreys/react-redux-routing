c0aff109f45ffd701348ea2755d182384124181b
Carl Humphreys <carlh.crimson@gmail.com>
2016-06-01 15:36:32 +0100

    initial data, reducers, action creators for dispatching

---
+++ src/reducers/comments.js
@@ -0,0 +1,14 @@
+function comments(store = [], action) {
+	switch (action.type) {
+		case 'ADD_COMMENT':
+			// add comment
+			break;
+		case 'REMOVE_COMMENT':
+			// delete comment
+			break;
+		default:
+			// no action defined
+			break;
+}
+
+export default comments;
---
+++ src/reducers/posts.js
@@ -0,0 +1,12 @@
+function posts(store = [], action) {
+	switch (action.type) {
+		case 'ADD_POST':
+			// add photo & caption, initialise likes & comment section?
+			break;
+		default:
+			// no action defined
+			break;
+	}
+}
+
+export default posts;
