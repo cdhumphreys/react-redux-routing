4caddff243ecb97f8dc036cb0b57390bf9009a18
Carl Humphreys <carlh.crimson@gmail.com>
2016-06-01 15:32:20 +0100

    adding react-redux module, adding data to be used as a base, making action creators for dispatching

--- src/index.js
+++ src/index.js
@@ -2,23 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import { Provider } from 'react-redux';
 
-//Needed for webpack to know to watch and compile 
+
+// import stylesheets
 import css from './stylesheet.scss';
 
 
-// path components
+// import components
 import Main from './components/Main.js';
 import PhotoGrid from './components/PhotoGrid.js';
 import Single from './components/Single.js';
 
+// import store and store history
+import store, { history } from './store.js';
+
+
 const routes = (
-	<Router history={browserHistory}>
-		<Route path="/" component={Main}>
-			<IndexRoute component={PhotoGrid}></IndexRoute>
-			<Route path="view/:postID" component={Single}></Route>			
-		</Route>
-		
-	</Router>	
+	<Provider store={store}>
+		<Router history={history}>
+			<Route path="/" component={Main}>
+				<IndexRoute component={PhotoGrid}></IndexRoute>
+				<Route path="view/:postID" component={Single}></Route>			
+			</Route>		
+		</Router>
+	</Provider>
 );
 ReactDOM.render(routes, document.getElementById('app'));
